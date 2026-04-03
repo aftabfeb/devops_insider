8b10b1e562b6c5908624f47e22c21749d6526f02
aftabfeb <aftab15feb@gmail.com>
2026-04-03 11:24:48 +0530

    committed

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.67.0"
+    }
+  }
+}
+// this is a provider block
+provider "azurerm" {
+    features {}
+  # Configuration options
+}
